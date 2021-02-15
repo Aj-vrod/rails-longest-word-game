891daff4b300b72209107f1879682117676e8620
Ammy_Varela <ammyvrodriguez@hotmail.com>
2021-02-15 16:47:52 +0100

    app running fine

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
     @letters = 10.times.map { ('A'..'Z').to_a.sample }
@@ -5,5 +8,23 @@ class GamesController < ApplicationController
 
   def score
     @suggestion = params[:suggestion]
+    @grid = params[:grid].split
+    url = "https://wagon-dictionary.herokuapp.com/#{@suggestion}"
+    @data = JSON.parse(open(url).read)
+    @score =
+      if @data['found']
+        if grid_included?
+          "Congratulations! #{@suggestion.upcase} is a valid English word"
+        else
+          "Sorry but #{@suggestion.upcase} does not seems to be a valid English word"
+
+        end
+      else
+        "Sorry but #{@suggestion.upcase} can't be built out of #{@grid.join(', ')}"
+      end
+  end
+
+  def grid_included?
+    @suggestion.upcase.chars.all? { |letter| @suggestion.count(letter) <= @grid.count(letter) }
   end
 end
